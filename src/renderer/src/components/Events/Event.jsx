73b8a6138d5eab67a4299a664796cd7e69e8a810
Joshua Rashtian <joshrashtian1@gmail.com>
2024-01-01 22:37:59 -0800

    Created Time Difer

--- src/renderer/src/components/Events/Event.jsx
+++ src/renderer/src/components/Events/Event.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { DateCalcuation, convertDate } from '../functions'
+import { TimeDefer } from '../time'
 
 const EventPanel = () => {
 
 
   const time = new Date(2024, 2, 10, 5, 30, 0)
   const sampleTime = new DateCalcuation(time)
+  const date2 = new Date(2024, 2, 10, 7, 10, 0)
+  const sampleTime2 = new DateCalcuation(date2)
+  const timeComp = new TimeDefer(time, date2)
 
   //const sampleTime = new DateCalcuation(startTime)
+  console.log(timeComp)
   console.log(sampleTime.printInfo())
   let sampleTags = ['Chemistry', 'Computer Science', '']
 
@@ -17,7 +22,7 @@ const EventPanel = () => {
             <h1 className=' font-eudoxusbold text-xl'>Bioinformatics</h1>
             <h1 className='font-eudoxus'>{sampleTime.fetchDate('long')}</h1>
             <ul className='p-2 bg-slate-100 w-[100%] rounded-xl'>
-                <p className='font-jet text-slate-500'>Time: {sampleTime.fetchTime()} | Location: Valencia</p>
+                <p className='font-jet text-slate-500'>Time: {sampleTime.fetchTime()} - {sampleTime2.fetchTime()} | Location: Valencia</p>
             </ul>
         </ul>
     </div>
---
+++ src/renderer/src/components/time.js
@@ -0,0 +1,33 @@
+export class TimeDefer {
+    constructor (time1, time2) {
+        this.one = time1
+        this.two = time2
+    }
+
+    timeOne() {
+        return this.one
+    }
+    timeTwo() {
+        return this.two
+    }
+
+    oneToString() {
+        return this.one.toDateString()
+    }
+
+    twoToString() {
+        return this.two.toDateString()
+    }
+
+    differHourMin() {
+        let hourDifference = this.two.getHours() - this.one.getHours()
+        let minDifference;
+
+        if (this.one.getMinutes() > this.two.getMinutes()) { 
+            minDifference = ((this.two.getMinutes() + 60) - this.one.getMinutes())
+            hourDifference--
+        }
+        else { minDifference = this.two.getMinutes() - this.one.getMinutes() }
+        return `${hourDifference}:${minDifference}`
+    }
+}
